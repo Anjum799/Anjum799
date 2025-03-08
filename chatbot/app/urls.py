cc348e19cbf60a99f7ce8fdc4b827919cb3a110f
Anjum <anjum9264u@gmail.com>
2025-03-08 10:43:10 +0530

    Delete chatbot directory

--- chatbot/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import chatbot_view,chat,chat_history
-
-urlpatterns=[
-    path("",chatbot_view,name="Chatbot"),
-    path("chat/",chat, name="chat"),
-    path("history/",chat_history,name="history")
-]
